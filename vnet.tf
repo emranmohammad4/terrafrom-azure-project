70fd3117ebeb6c855673b1be5fb1ac2643646549
emranmohammad4 <emranmohammad4@gmail.com>
2023-03-10 13:12:04 -0500

    nsg changes

--- vnet.tf
+++ vnet.tf
@@ -16,3 +16,13 @@ resource "azurerm_subnet" "web-subnet" {
   address_prefixes     = ["10.0.1.0/24"]
 
 }
+
+#network security group 
+resource "azurerm_network_security_group" "web-nsg" {
+  name                = "web-nsg"
+  location            = azurerm_resource_group.web-rg.location
+  resource_group_name = azurerm_resource_group.web-rg.name
+  tags = {
+    environment = "dev"
+  }
+}
